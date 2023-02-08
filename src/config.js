881b7733b59f6bd8791209f3f758b5b60309083f
Adam Webb <adam.webb@mrjaffesclass.com>
2023-02-08 12:46:32 -0800

    Step 19 (almost finished)

---
+++ src/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAuaqudCRAybjjTtgAC8BMJpTefCMmo6wQ",
+    authDomain: "chatroom-69ffb.firebaseapp.com",
+    databaseURL: "https://chatroom-69ffb-default-rtdb.firebaseio.com",
+    projectId: "chatroom-69ffb",
+    storageBucket: "chatroom-69ffb.appspot.com",
+    messagingSenderId: "919686774864",
+    appId: "1:919686774864:web:e3e8a08ad701b2b3e31155"
+  };
+
+  export default firebaseConfig;
